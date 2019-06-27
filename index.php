ea8b6467a4ccf1e15064d123113e482dcd74fd32
Richard Angapin <rangapin@gmx.com>
2019-06-28 02:23:14 +0800

    firstdraft

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>FreediveTube</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+
+<body>
+  <div id="pageContainer">
+    <div id="headContainer">
+    </div>
+
+    <div id="sideContainer">
+    </div>
+
+    <div id="mainSectionContainer">
+    </div>
+  </div>
+</body>
+
+</html>
