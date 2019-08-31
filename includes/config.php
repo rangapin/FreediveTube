fac885338a28f85b5ab67bd5a4ed5995c060ffea
Richard Angapin <rangapin@gmx.com>
2019-08-31 17:49:00 +0800

    live setup

--- includes/config.php
+++ includes/config.php
@@ -2,10 +2,11 @@
 ob_start(); //Turns on output buffering
 session_start();
 
-date_default_timezone_set("Asia/Bangkok");
+date_default_timezone_set("Europe/Prague");
 
 try {
-    $con = new PDO("mysql:dbname=freefhzn_freedivetube;host=localhost", "freefhzn_rangapin", "db_password");
+    $con = new PDO("mysql:dbname=FreediveTube;host=localhost", "root", "");
+  //$con = new PDO("mysql:dbname=freefhzn_freedivetube;host=localhost", "freefhzn_rangapin", "db_password");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch (PDOException $e) {
