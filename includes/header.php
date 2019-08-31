702ec7b0b21197979fb3be1fc64634681e22ffe6
Richard Angapin <rangapin@gmx.com>
2019-08-31 17:51:37 +0800

    logodisplay

--- includes/header.php
+++ includes/header.php
@@ -18,14 +18,15 @@ $userLoggedInObj = new User($con, $usernameLoggedIn);
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="icon" type ="image/png" href="../assets/images/icons/new_favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Submerged yourself into your favorite underwater videos, dive into a world of wonder and silence, upload and comments on your friends best GoPro videos. Welcome to FreediveTube!">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="assets/js/commonActions.js"></script>
     <script src="assets/js/userActions.js"></script>
-    <meta name="description" content="Submerged yourself into your favorite underwater videos. Welcome to FreediveTube!">
 
 </head>
 <body>
@@ -38,7 +39,7 @@ $userLoggedInObj = new User($con, $usernameLoggedIn);
             </button>
 
             <a class="logoContainer" href="index.php">
-                <img src="assets/images/icons/newlogo.png" alt="Site logo">
+                <img src="assets/images/icons/logo_transparent.png" alt="Site logo">
             </a>
 
             <div class="sm searchBarContainer">
